9d75b17d8dad9231d7815d4570f557400263cdf9
XO <xiangbolong@gmail.com>
2019-08-06 20:21:56 +0800

    测试

--- Sampler/ViewController.swift
+++ Sampler/ViewController.swift
@@ -60,6 +60,55 @@ extension ViewController {
         _locationManager?.startUpdatingLocation()
     }
     
+    func __savePhotoAndGetphoto(imageData: Data) {
+        var localID: String!//标识符
+        
+        PHPhotoLibrary.shared().performChanges({
+            let creationRequest = PHAssetCreationRequest.forAsset()
+            creationRequest.addResource(with: .photo, data: imageData, options: nil)
+            let assetPlaceholder = creationRequest.placeholderForCreatedAsset
+            //保存标识符
+            localID = assetPlaceholder?.localIdentifier
+        }) { (succ, error) in
+            if succ {
+                debugPrint("保存成功")
+                //通过标识符获取对应的资源
+                let assetResult = PHAsset.fetchAssets(withLocalIdentifiers: [localID], options: nil)
+                let asset = assetResult[0]
+                let options = PHContentEditingInputRequestOptions()
+                options.canHandleAdjustmentData = {(adjustmeta: PHAdjustmentData) -> Bool in
+                    return true
+                }
+                //获取保存的图片路径
+                PHImageManager.default().requestImageData(for: asset, options: nil, resultHandler: { (imageData, dataUTI, orientation, info) in
+                    guard let imageData = imageData else {
+                        return
+                    }
+                    
+                })
+//                asset.requestContentEditingInput(with: options, completionHandler: { (contentEditingInput: PHContentEditingInput?, info: [AnyHashable : Any]) in
+//                    let file = contentEditingInput!.fullSizeImageURL!
+//                    print("地址：\(file)")
+//                })
+                
+//                //获取保存的原图
+//                PHImageManager.default().requestImage(for: asset, targetSize: PHImageManagerMaximumSize, contentMode: PHImageContentMode.aspectFit, options: nil, resultHandler: { (image, handle: [AnyHashable : Any]?) in
+//                    print("获取原图成功：\(String(describing: image))")
+//                })
+//
+//                //获取保存的缩略图
+//                PHImageManager.default().requestImage(for: asset, targetSize: CGSize.init(width: 100, height: 100), contentMode: PHImageContentMode.aspectFit, options: nil, resultHandler: { (image, handle: [AnyHashable : Any]?) in
+//                    print("获取缩略图成功：\(String(describing: image))")
+//                })
+                
+            } else {
+                print("保存失败-->\(error!.localizedDescription)")
+            }
+        }
+    }
+    
+
+    
 }
 
 extension ViewController: CLLocationManagerDelegate {
@@ -105,10 +154,22 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
             guard let location = _locationManager?.location else {
                 return
             }
-            
-            guard let imageData = image.writeExif(mediaMetadata: mediaMetadata, location: location) else {
+            let wImage = image.waterMarkedImage(location: location)
+            guard let imageData = wImage.writeExif(mediaMetadata: mediaMetadata, location: location) else {
                 return
             }
+//            let ciImgae = CIImage(data: imageData)!
+//            let newimage = UIImage(ciImage: ciImgae)
+//            let t = UIImage(data: imageData)!
+//            PHPhotoLibrary.shared().performChanges({
+//                debugPrint(newimage)
+//                let creationRequest = PHAssetCreationRequest.creationRequestForAsset(from: t)
+//                creationRequest.location = location
+//                creationRequest.creationDate = Date()
+////                let assetPlaceholder = creationRequest.placeholderForCreatedAsset
+//            }) { (su, ero) in
+//                debugPrint("保存结果", su)
+//            }
             writeExifImageDataToSavedPhotosAlbum(imageData, location: location, creationDate: Date()) { (result) in
                 switch result {
                 case let .success(localIdentifier):
@@ -119,11 +180,19 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
                         return true
                     }
                     //获取保存的图片路径
-                    asset.requestContentEditingInput(with: options, completionHandler: { (contentEditingInput: PHContentEditingInput?, info: [AnyHashable : Any]) in
-                        let file = contentEditingInput!.fullSizeImageURL!
-                        let info = readImageExif(file: file)
-                        debugPrint("读取到的信息", info)
+//                    asset.requestContentEditingInput(with: options, completionHandler: { (contentEditingInput: PHContentEditingInput?, info: [AnyHashable : Any]) in
+//                        let file = contentEditingInput!.fullSizeImageURL!
+//                        let info = readImageExif(file: file)
+//                        debugPrint("读取到的信息", info)
+//                    })
+                    PHImageManager.default().requestImageData(for: asset, options: nil, resultHandler: { (imageData, dataUTI, orientation, info) in
+                        guard let imageData = imageData else {
+                            return
+                        }
+                        let cImage = CIImage(data: imageData)
+                        debugPrint("读取到的信息", cImage?.properties)
                     })
+
                     break
                 case let .failure(error):
                     break
