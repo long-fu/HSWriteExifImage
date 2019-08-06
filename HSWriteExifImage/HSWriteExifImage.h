d4b3e7fc61169a3b5f1bbfd93b0969b57eb8bcc0
XO <xiangbolong@gmail.com>
2019-08-06 11:12:16 +0800

    Initial Commit

---
+++ HSWriteExifImage/HSWriteExifImage.h
@@ -0,0 +1,19 @@
+//
+//  HSWriteExifImage.h
+//  HSWriteExifImage
+//
+//  Created by luo fengyuan on 2019/8/6.
+//  Copyright © 2019 onelcat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HSWriteExifImage.
+FOUNDATION_EXPORT double HSWriteExifImageVersionNumber;
+
+//! Project version string for HSWriteExifImage.
+FOUNDATION_EXPORT const unsigned char HSWriteExifImageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HSWriteExifImage/PublicHeader.h>
+
+
